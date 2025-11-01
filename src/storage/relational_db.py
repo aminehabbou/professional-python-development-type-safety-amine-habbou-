3cd537852e52af867efb38a808f6562749cbd1a9
aminehabbou <amineh11@outlook.com>
2025-11-01 19:47:26 +0100

    create author and article models, then create tables in db

---
+++ src/storage/relational_db.py
@@ -0,0 +1,13 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import DeclarativeBase, sessionmaker
+
+data_base_url = "mysql+pymysql://root:secret@localhost:3306/pyhton-de"
+
+engine = create_engine(data_base_url, echo=True, pool_size=5, max_overflow=10)
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+Session = sessionmaker(bind=engine)
