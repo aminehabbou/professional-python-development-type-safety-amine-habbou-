9b1e1fd1e2930de12c512766491408c8f6656816
aminehabbou <amineh11@outlook.com>
2025-11-10 02:55:09 +0100

    update mongo documents

--- src/usecases/export_articles.py
+++ src/usecases/export_articles.py
@@ -2,6 +2,7 @@ import storage.mongo  # noqa: F401
 from models.mongo import Author as MongoAuthor
 from models.mongo import ScientificArticle as MongoArticle
 from models.relational import ScientificArticle
+from mongoengine import DoesNotExist
 from sqlalchemy import select
 from storage.relational_db import Session
 
@@ -16,16 +17,28 @@ def export_from_db() -> None:
                 full_name=article.author.full_name,
                 author_title=article.author.title,
             )
-            m_article = MongoArticle(
-                db_id=article.id,
-                title=article.title,
-                summary=article.summary,
-                file_path=article.file_path,
-                created_at=article.created_at,
-                arxiv_id=article.arxiv_id,
-                author=m_author,
-            )
-            m_article.save()
+            try:
+                m_article = MongoArticle.get.objects(arxiv_id=article.arxiv_id)
+                m_article.update(
+                    db_id=article.id,
+                    title=article.title,
+                    summary=article.summary,
+                    file_path=article.file_path,
+                    created_at=article.created_at,
+                    arxiv_id=article.arxiv_id,
+                    author=m_author,
+                )
+            except DoesNotExist:
+                m_article = MongoArticle(
+                    db_id=article.id,
+                    title=article.title,
+                    summary=article.summary,
+                    file_path=article.file_path,
+                    created_at=article.created_at,
+                    arxiv_id=article.arxiv_id,
+                    author=m_author,
+                )
+                m_article.save()
 
 
 if __name__ == "__main__":
