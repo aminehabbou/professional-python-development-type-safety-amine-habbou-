a64978539a805e5030348c328ac23e4a97f056d2
aminehabbou <amineh11@outlook.com>
2025-11-14 14:19:59 +0100

    implemeting the pipeline with pandas

--- src/main.py
+++ src/main.py
@@ -1,12 +1,17 @@
-from pathlib import Path
-
-from usecases.export_articles import export_from_db
-from usecases.import_articles import load_data_from_csv
+from usecases.export_articles import create_in_mongo
+from usecases.import_articles import create_in_relational_db, load_data_from_csv
 from usecases.search_text import search_text_index
 
 if __name__ == "__main__":
-    load_data_from_csv(Path("data/articles.csv"))
-    export_from_db()
+    df = (
+        load_data_from_csv("data/articles.csv")
+        .pipe(create_in_relational_db)
+        .pipe(create_in_mongo)
+    )
+    print("DataFrame after importing to relational db and exporting to mongo:")
+    print(df)
+
     results = search_text_index("low-frequency correlators")
+    print("len results:", len(results))
     for ar in results:
         print(f"{ar['arxiv_id']}: {ar['title']} (score={ar['text_score']:.3f})")
